5d21f591501b7565d16833100c06737f2dcfed88
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-28 11:59:36 +0300

    indakwa portfolio

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import "./nav.css";
 import Logo from "../../Images/Logo.png";
-
+import {BiMenuAltRight} from 'react-icons/bi'
+import {FaWindowClose} from 'react-icons/fa'
 function Navbar() {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
   return (
     <>
       <nav className="nav">
@@ -34,9 +45,43 @@ function Navbar() {
                 <p>Contact</p>
               </a>
             </div>
+
+            
           </div>
+
+          <div className='menu-div' onClick={openSidebar}>
+
+                <BiMenuAltRight className='menu'/>
+
+            </div>
         </div>
       </nav>
+
+      {isSidebarOpen &&(
+        <aside className="aside active">
+
+        <div className="close-btn" onClick={closeSidebar}>
+
+            <FaWindowClose className='close'/>
+
+        </div>
+
+        <div className="aside-container">
+
+            <div className="aside-links"><a href='#home'>Home</a></div>
+            <div className="aside-links"><a href='#about'>About</a></div>
+            <div className="aside-links"><a href="#services">Services</a></div>
+            <div className="aside-links"><a href="#contact">Contact</a></div>
+
+
+
+
+        </div>
+
+
+
+      </aside>
+      )}
     </>
   );
 }
