dfd2c9831adef166403a84be891b1bced5c59419
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-28 15:50:44 +0300

    indakwa portfolio

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -24,7 +24,13 @@ function About(){
 
                             <div className='about-details'>
 
-                                <h1>Who is Eugine Indakwa?</h1>
+                                <h1 className='about-details-intro'>Who is Eugine Indakwa?</h1>
+
+                                <p className='eu-desc'>&#x2BC1; Hey, I am Eugine Indakwa, a versatile individual who's deeply interested in the intricate tapestry of the legal world. </p>
+
+                                <p className='eu-desc-2'>&#x2BC1; My areas of interest span across various legal domains, and I'm well equipped to provide legal advice, conduct in-depth research, and produce well-articulated legal content.</p>
+
+                                <p className='eu-desc-3'> &#x2BC1;However, it's essential to clarify that I'm not a practicing advocate, and everything shared on this website represents my personal opinion as a non-advocate. </p>
 
 
                             </div>
