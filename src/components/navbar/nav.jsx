3f431e16668526285405cc4ebecdfa9fe43de68a
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-28 11:00:29 +0300

    indakwa portfolio

--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import './nav.css'
-import Logo from '../../Images/Logo.png'
+import React from "react";
+import "./nav.css";
+import Logo from "../../Images/Logo.png";
 
+function Navbar() {
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav-container">
+          <div className="site-logo">
+            <img src={Logo} alt="logo" className="logo" />
+          </div>
 
-
-function Navbar (){
-    return(
-
-        <>
-
-            <nav className='nav'>
-
-                <div className='nav-container'>
-
-                <div className='site-logo'>
-
-                    <img src={Logo} alt='logo' className='logo'/>
-
-
-                </div>
-
-                <div className='nav-links'>
-
-                    <div className='home'><a href='#home' className='home-p' ><p >Home</p></a></div>
-                    <div className='home'><a href='#about' className='about'><p >About</p></a></div>
-                    <div className='home'><a href='#services' className='service'><p >Services</p></a></div>
-                    <div className='home'><a href='#contact' className='contact'><p >Contact</p></a></div>
-
-
-                </div>
-
-                </div>
-
-
-
-
-            </nav>
-        
-        
-        </>
-    )
+          <div className="nav-links">
+            <div className="home">
+              <a href="#home" className="home-p">
+                <p>Home</p>
+              </a>
+            </div>
+            <div className="home">
+              <a href="#about" className="about">
+                <p>About</p>
+              </a>
+            </div>
+            <div className="home">
+              <a href="#services" className="service">
+                <p>Services</p>
+              </a>
+            </div>
+            <div className="home">
+              <a href="#contact" className="contact">
+                <p>Contact</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
