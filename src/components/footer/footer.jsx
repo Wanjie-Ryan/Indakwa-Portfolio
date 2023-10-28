93f7aca2f4cfe4d55edc714b6338920b466e9cc7
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-28 22:27:53 +0300

    indakwa portfolio

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,57 +1,39 @@
-import React from 'react'
-import './footer.css'
-import {BsLinkedin, BsTwitter} from 'react-icons/bs'
+import React from "react";
+import "./footer.css";
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
 
+function Footer() {
+  return (
+    <>
+      <section className="footer">
+        <div className="footer-container">
+          <div className="footer-details">
+            <p className="footer-name">
+              <i>Eugine Mushieni Indakwa</i>{" "}
+            </p>
 
+            <p className="footer-email">indakwa@gmail.com</p>
 
-function Footer(){
+            <div className="footer-links">
+              <a
+                href="https://www.linkedin.com/in/eugine-mushieni-indakwa-8702b5244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                target="_blank"
+                rel="noreferrer"
+                className="linked"
+              >
+                <BsLinkedin />
+              </a>
+            </div>
+          </div>
 
-    return(
+          <div className="footer-logo"></div>
 
-        <>
-
-            <section className='footer'>
-
-                <div className='footer-container'>
-
-                    <div className='footer-details'>
-
-                        <p className='footer-name'><i>Eugine Mushieni Indakwa</i> </p>
-
-                        <p className='footer-email'>indakwa@gmail.com</p>
-
-                        <div className='footer-links'>
-
-                        <a href='https://www.linkedin.com/in/eugine-mushieni-indakwa-8702b5244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' target ='_blank' rel="noreferrer">
-
-<BsLinkedin/>
-</a>
-
-
-                        </div>
-
-                    </div>
-
-
-                    <div className='footer-logo'>
-
-                    </div>
-
-                    <div className='footer-design'>
-
-
-                    </div>
-
-
-                </div>
-
-
-
-            </section>
-        
-        
-        </>
-    )
+          <div className="footer-design"></div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
