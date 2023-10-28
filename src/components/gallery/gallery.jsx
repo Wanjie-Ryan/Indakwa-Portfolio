cdaaf76d1081037db14f11780c9761b2456f2c7c
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-28 17:22:46 +0300

    indakwa portfolio

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,105 +1,88 @@
-import React from 'react'
-import './gallery.css'
+import React from "react";
+import "./gallery.css";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
+function Gallery() {
+  const imageData = [
+    {
+      id: 1,
+      image: require("../../Images/Indak-1.jpeg"),
+    },
 
-function Gallery(){
+    {
+      id: 2,
+      image: require("../../Images/Indak-2.jpeg"),
+    },
 
+    {
+      id: 3,
+      image: require("../../Images/Indak-3.jpeg"),
+    },
 
-    const imageData = [
-        {
-          id: 1,
-          image: require("../../Images/Indak-1.jpeg"),
-         
-        },
-    
-        {
-          id: 2,
-          image: require("../../Images/Indak-2.jpeg")
-         
-        },
-    
-        {
-          id: 3,
-          image: require("../../Images/Indak-3.jpeg")
-         
-        },
-    
-        {
-          id: 4,
-          image: require("../../Images/Indak-4.jpeg")
-          
-        },
-    
-        {
-          id: 5,
-          image: require("../../Images/Indak-5.jpeg")
-          
-        },
-    
-        {
-          id: 6,
-          image: require("../../Images/Indak-6.jpeg")
-          
-        },
-    
-        {
-          id: 7,
-          image: require("../../Images/Indak-7.jpeg")
-          
-        },
-    
-        {
-          id: 8,
-          image: require("../../Images/Indak-8.jpeg")
-          
-        },
-    
-        {
-          id: 9,
-          image: require("../../Images/Indak-9.jpeg")
-          
-        },
-    
-        {
-          id: 10,
-          image: require("../../Images/Indak-10.jpeg")
-        },
+    {
+      id: 4,
+      image: require("../../Images/Indak-4.jpeg"),
+    },
 
-        {
-            id: 11,
-            image: require("../../Images/Indak.jpeg")
-          },
-    ]
+    {
+      id: 5,
+      image: require("../../Images/Indak-5.jpeg"),
+    },
 
-    return(
-        <>
+    {
+      id: 6,
+      image: require("../../Images/Indak-6.jpeg"),
+    },
 
-            <section className='gallery'>
+    {
+      id: 7,
+      image: require("../../Images/Indak-7.jpeg"),
+    },
 
-            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 768: 3, 992: 4 }}>
-        <Masonry gutter="1rem">
-          {imageData.map((item, index) => (
-            <img
-              className="masonry__img"
-              src={item.image}
-              key={index}
-              alt=""
-              style={{ width: "100%", display: "block", borderRadius: "10px" }}
-            />
-          ))}
-        </Masonry>
-      </ResponsiveMasonry>
+    {
+      id: 8,
+      image: require("../../Images/Indak-8.jpeg"),
+    },
 
+    {
+      id: 9,
+      image: require("../../Images/Indak-9.jpeg"),
+    },
 
+    {
+      id: 10,
+      image: require("../../Images/Indak-10.jpeg"),
+    },
 
+    {
+      id: 11,
+      image: require("../../Images/Indak.jpeg"),
+    },
+  ];
 
-
-            </section>
-
-
-        </>
-    )
+  return (
+    <>
+      <section className="gallery">
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 768: 3, 992: 4 }}>
+          <Masonry gutter="1rem">
+            {imageData.map((item, index) => (
+              <img
+                className="masonry__img"
+                src={item.image}
+                key={index}
+                alt=""
+                style={{
+                  width: "100%",
+                  display: "block",
+                  borderRadius: "12px",
+                }}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </section>
+    </>
+  );
 }
 
-export default Gallery
+export default Gallery;
