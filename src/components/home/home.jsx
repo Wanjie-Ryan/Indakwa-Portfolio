91e4e9396d00985356943248b7b2aa83dfabe7e8
Ryan <ryanwanjiedistortion@gmail.com>
2023-10-28 12:52:57 +0300

    indakwa portfolio

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,54 +1,32 @@
-import React from 'react'
-import './home.css'
-import Indakwa from '../../Images/Indak-8.jpeg'
+import React from "react";
+import "./home.css";
+import Indakwa from "../../Images/Indak-8.jpeg";
 
+function Home() {
+  return (
+    <>
+      <section id="home" className="home-section">
+        <div className="home-container">
+          <div className="home-description">
+            <h1 className="h1-intro">Eugine Mushieni Indakwa</h1>
 
-function Home(){
+            <p className="p-desc">
+              Informed Perspectives:Mushieni's Law & Policy Chronicoles{" "}
+            </p>
 
-    return(
-        <>
+            <p className="p-desc-law">Let's Explore the vast laws together. </p>
+          </div>
 
-        <section id='home' className='home-section'>
-
-            <div className='home-container'>
-
-                <div className='home-description'>
-
-                    <h1 className='h1-intro'>Eugine Mushieni Indakwa</h1>
-
-                    <p className='p-desc'>Informed Perspectives:Mushieni's Law & Policy Chronicoles </p>
-
-                    <p className='p-desc-law'>Let's Explore the vast laws together. </p>
-
-
-
-
-                </div>
-
-                <div className='image-intro'>
-
-                    <div className='image-shadow'>
-
-                        <div className='shadow'>
-
-                        </div>
-                        <img src={Indakwa} alt='' className='intro-image'/>
-                    </div>
-
-
-                </div>
-
-
-
+          <div className="image-intro">
+            <div className="image-shadow">
+              <div className="shadow"></div>
+              <img src={Indakwa} alt="" className="intro-image" />
             </div>
-
-
-
-        </section>
-
-
-        </>
-    )
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
